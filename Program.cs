63fa2e302861c6047367c982309b1913a6d74fdd
guh-alves9 <gustavo-teixeira2002@hotmail.com>
2022-10-13 15:34:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio6
+{
+    class Program
+    {
+       public static double Potencia(int x, ref int y)
+        {
+            double p = 0;
+            p = Math.Pow(x, y);
+            return p;        
+        }
+ 
+        static void Main(string[] args)
+        {
+          int x, y;
+          Console.WriteLine("Digite um numero: ");
+          x = int.Parse(Console.ReadLine());
+          Console.WriteLine("Digite outro numero: ");
+          y = int.Parse(Console.ReadLine());
+          Console.WriteLine(Potencia(x, ref y));
+
+        }
+    }
+}
